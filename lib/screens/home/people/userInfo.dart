927bca21d054074828452aef744dcd8262669145
NourahALfulaij <nmw.2111@gmail.com>
2023-01-14 22:40:03 +0300

    userInfo page Done

--- lib/screens/home/people/userInfo.dart
+++ lib/screens/home/people/userInfo.dart
@@ -2,10 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:ices2023/screens/home/people/person.dart';
 import 'package:ices2023/config/palette.dart';
+import 'package:intl/intl.dart';
 
 class UserInformation extends StatelessWidget {
   // UserInformation(DocumentSnapshot<Object?> documentSnapshot);
-
+  String formattedDate(timeStamp){
+    var dateFromTimeStamp=DateTime.fromMillisecondsSinceEpoch(timeStamp.seconds *1000);
+    return DateFormat('dd-MM-yyyy').format(dateFromTimeStamp);
+  }
   final DocumentSnapshot documentSnapshot;
   UserInformation(this.documentSnapshot);
   @override
@@ -21,28 +25,67 @@ class UserInformation extends StatelessWidget {
             Container(
               width: 100,
               height:200,
-              child:Text("Person Information",textAlign:TextAlign.center ,
-                style: TextStyle(fontSize:36 , fontStyle:FontStyle.normal , color:Colors.white ),
-              ),
               color: Palette.yellow,
-            ),
-            Container(
-              width: 100,
-              height:200,
-              child:Text("Paper Name",textAlign:TextAlign.center ,
-                style: TextStyle(fontSize:36 , fontStyle:FontStyle.normal ,color:Colors.white ),
-              ),
-              color: Palette.green,
+              child:Column(
+
+                children:[
+                  Text("Person Information",textAlign:TextAlign.center ,
+                  style: TextStyle(fontSize:36 , fontStyle:FontStyle.normal , color:Palette.darkBlue,fontFamily: 'OpenSans' ,fontWeight: FontWeight.bold ),
+
+                ),
+                  Text(documentSnapshot.get("name"),textAlign:TextAlign.center ,
+                    style: TextStyle(fontSize:19 , fontStyle:FontStyle.normal , color:Colors.white,fontFamily: 'OpenSans' ),
+
+                  ),
+                  Text(documentSnapshot.get("organization"),textAlign:TextAlign.center ,
+                    style: TextStyle(fontSize:19 , fontStyle:FontStyle.normal , color:Colors.white ,fontFamily: 'OpenSans'),
+
+                  ),
+
+
+          ],
+
+              )
+
 
             ),
             Container(
               width: 100,
               height:200,
-              child:Text("Presentation Subject",textAlign:TextAlign.center ,
-                style: TextStyle(fontSize:36 , fontStyle:FontStyle.normal ,color:Colors.white  ),
-              ),
-              color: Palette.blue,
+                color: Palette.green,
+              child:Column(
 
+                children:[
+              Text("Paper Name",textAlign:TextAlign.center ,
+                style: TextStyle(fontSize:36 , fontStyle:FontStyle.normal ,color:Palette.darkBlue,fontFamily: 'OpenSans',fontWeight: FontWeight.bold ),
+              ),
+                  Text(formattedDate(documentSnapshot.get("date")),textAlign:TextAlign.center ,
+                    style: TextStyle(fontSize:19 , fontStyle:FontStyle.normal , color:Colors.white ,fontFamily: 'OpenSans'),
+                  ),
+                  Text(documentSnapshot.get("paperName "),textAlign:TextAlign.center ,
+                    style: TextStyle(fontSize:19 , fontStyle:FontStyle.normal , color:Colors.white,fontFamily: 'OpenSans' ),
+                  ),
+              ]
+            )
+            ),
+            Container(
+              width: 100,
+              height:200,
+                color: Palette.blue,
+              child:Column(
+
+                children:[
+              Text("Presentation Subject",textAlign:TextAlign.center ,
+                style: TextStyle(fontSize:36 , fontStyle:FontStyle.normal ,color:Palette.darkBlue ,fontFamily: 'OpenSans' ,fontWeight: FontWeight.bold ),
+              ),
+                  Text(formattedDate(documentSnapshot.get("date")),textAlign:TextAlign.center ,
+                    style: TextStyle(fontSize:19 , fontStyle:FontStyle.normal , color:Colors.white ,fontFamily: 'OpenSans'),
+                  ),
+                  Text(documentSnapshot.get("PresentationName"),textAlign:TextAlign.center ,
+                    style: TextStyle(fontSize:19 , fontStyle:FontStyle.normal , color:Colors.white,fontFamily: 'OpenSans' ),
+                  ),
+]
+              )
             ),
           ],
         )
